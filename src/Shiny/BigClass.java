2b472780c45483f3ceb6cd6467a4888f54064263
WernCheen Tye <s3647030@student.rmit.edu.au>
2017-04-10 11:03:08 +1000

    Errors Coding

--- src/Shiny/BigClass.java
+++ src/Shiny/BigClass.java
@@ -19,4 +19,39 @@ public class BigClass {
 		System.out.print("Too much girlfriends, can't complain");
 		
 	}
+	
+	public void complainAboutMyFuckingLife() {
+		System.out.println("It has been years I haven't got a girlfriend");
+	}
+	
+	public void MayBeComplain () {
+		System.out.print("Too much girlfriends and boyfriends, too complicated");
+	}
+	
+	public void NoMoreComplaining () {
+		System.out.print("Too much girlfriends, can't complain");
+		
+	}	public void complainAboutMyFuckingLife() {
+		System.out.println("It has been years I haven't got a girlfriend");
+	}
+	
+	public void MayBeComplain () {
+		System.out.print("Too much girlfriends and boyfriends, too complicated");
+	}
+	
+	public void NoMoreComplaining () {
+		System.out.print("Too much girlfriends, can't complain");
+		
+	}	public void complainAboutMyFuckingLife() {
+		System.out.println("It has been years I haven't got a girlfriend");
+	}
+	
+	public void MayBeComplain () {
+		System.out.print("Too much girlfriends and boyfriends, too complicated");
+	}
+	
+	public void NoMoreComplaining () {
+		System.out.print("Too much girlfriends, can't complain");
+		
+	}
 }
